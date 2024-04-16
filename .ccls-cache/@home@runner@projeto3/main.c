d9b3eef29a45c741e164dc54e817e09c594734bc
Manga10p <110128138+rotkis@users.noreply.github.com>
2024-04-16 23:28:11 +0000

    adiciona o maximo de 255 contatos na agenda

--- .ccls-cache/@home@runner@projeto3/main.c
+++ .ccls-cache/@home@runner@projeto3/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "contatos.h"
 
 int main(){
--- contatos.c
+++ contatos.c
@@ -1,53 +1,40 @@
 #include "contatos.h" //incluir o arquivo contatos.h
 #include <stdio.h>
+#include <string.h>
 
 ERROS criar(Agenda contatos[], int *pos) {
   if (*pos >= TOTAL)
     return MAX_CONTATO;
-
-  printf("Digite o nome: ");//NOME
-  fgets(contatos[*pos].nome, NOME, stdin);
-contatos[*pos].nome[strcspn(contatos[*pos].nome, "\n")] = '\0';
   clearBuffer();
+  printf("Digite o nome: "); // NOME
+  fgets(contatos[*pos].nome, NOME, stdin);
+  contatos[*pos].nome[strcspn(contatos[*pos].nome, "\n")] = '\0';
 
-    printf("Digite o sobrenome: ");
-    fgets(contatos[*pos].sobrenome, NOME, stdin);
+  printf("Digite o sobrenome: ");
+  fgets(contatos[*pos].sobrenome, SOBRENOME, stdin);
   contatos[*pos].sobrenome[strcspn(contatos[*pos].sobrenome, "\n")] = '\0';
-    clearBuffer();
 
-    printf("Digite seu Telefone: ");
-  fgets(contatos[*pos].telefone, NOME, stdin);
+  printf("Digite seu Telefone: ");
+  fgets(contatos[*pos].telefone, TELEFONE, stdin);
   contatos[*pos].telefone[strcspn(contatos[*pos].telefone, "\n")] = '\0';
-    clearBuffer();
 
-   printf("Digite seu Email: ");
-  fgets(contatos[*pos].email, NOME, stdin);
+  printf("Digite seu Email: ");
+  fgets(contatos[*pos].email, EMAIL, stdin);
   contatos[*pos].email[strcspn(contatos[*pos].email, "\n")] = '\0';
-    clearBuffer();
 
   *pos = *pos + 1;
   return OK;
 }
-ERROS deletar(Agenda contatos[], int *pos){
- 
-}
+ERROS deletar(Agenda contatos[], int *pos) {}
 
+ERROS listar(Agenda contatos[], int *pos) {}
 
-ERROS listar(Agenda contatos[], int *pos){
-  
-}
+ERROS salvar(Agenda contatos[], int *pos) {}
 
-ERROS salvar(Agenda contatos[], int *pos){
-  
-}
+ERROS carregar(Agenda contatos[], int *pos) {}
 
-ERROS carregar(Agenda contatos[], int *pos){
-  
-}
-
-
-
-void clearBuffer(){
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+void clearBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
 }
